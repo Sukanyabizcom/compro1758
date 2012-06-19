154516fdd36e2b8bc5f3f9a316095e0bea93b66b
sukanya paenphat <su_sukanya2011@hotmail.com>
2012-06-19 14:24:23 +0700

    เพิ่ม Programming is fun

--- src/th/ac/nu/student/u54341758/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54341758/compro/ch01/HelloWorld.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package th.ac.nu.student.u54341758.compro.ch01;
 
-/**
- *
- * @author com
- */
 public class HelloWorld {
-    
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Programmin is fun.");
+        
+    }
 }
