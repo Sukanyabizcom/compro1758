5a080f26ade05eb05f9cea517d52837b654dcf61
sukanya paenphat <su_sukanya2011@hotmail.com>
2012-06-19 14:00:00 +0700

    first commit

---
+++ src/compro1758/Compro1758.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compro1758;
+
+/**
+ *
+ * @author com
+ */
+public class Compro1758 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
