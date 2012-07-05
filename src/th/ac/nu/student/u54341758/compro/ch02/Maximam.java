b147a191c81d4c68adbefccbec452f2746b92146
sukanya paenphat <su_sukanya2011@hotmail.com>
2012-07-05 22:50:32 +0700

    ArrayDelaration

---
+++ src/th/ac/nu/student/u54341758/compro/ch02/Maximam.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341758.compro.ch02;
+
+
+public class Maximam {
+    public static void main(String []args){
+        int num = 2147483647;
+        System.out.println(num);
+        System.out.println(num +1);
+        int num2 = -2147483648;
+        System.out.println(num2-1);
+    }
+}
---
+++ src/th/ac/nu/student/u54341758/compro/ch02/NewClass.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341758.compro.ch02;
+
+public class NewClass {
+    public static void main (String[] args){
+        System.out.println(1+1+1+1*0);
+    }
+}
