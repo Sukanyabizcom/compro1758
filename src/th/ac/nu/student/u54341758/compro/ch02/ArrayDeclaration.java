c375d7f49740cbdef490fdfb1ac85db6bbe2ec49
sukanya paenphat <su_sukanya2011@hotmail.com>
2012-07-10 15:52:46 +0700

    Arrayprinter

--- src/th/ac/nu/student/u54341758/compro/ch02/ArrayDeclaration.java
+++
@@ -1,9 +0,0 @@
-
-package th.ac.nu.student.u54341758.compro.ch02;
-public class ArrayDeclaration {
-    public static void main(String[] args){
-        int numbers[] = {1,2,3,4,5,8,13};
-        System.out.println("First number of this array is " + numbers[0]);
-        System.out.println("Last number of this array is " + numbers[numbers.length - 1]);
-    }
-}
